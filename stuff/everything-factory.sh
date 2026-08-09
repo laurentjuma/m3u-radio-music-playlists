75f830f347bc04855aca8840e590aa0c42198fda
U-DESKTOP-3VIS1T9\junguler <junguler@yahoo.com>
2026-08-09 17:03:13 +0330

    update repo scripts to local versions

--- stuff/everything-factory.sh
+++ stuff/everything-factory.sh
@@ -10,7 +10,7 @@ for i in $(ls -v) ; do echo '#PLAYLIST: '$i | cat - $i | sed 's/#EXTM3U//g' | aw
 
 # add #EXTM3U to the first line and change the text file to a m3u file format
 #cat everything-full.txt | awk '!seen[$0]++' | grep -B1 "http" | grep -A1 "EXTINF" | awk 'length>4' | sed '1s/^/#EXTM3U\n/' > everything-full.m3u
-cat everything-full.txt | awk '!seen[$0]++' | sed '1s/^/#EXTM3U\n/' > everything-full.m3u
+cat everything-full.txt | grep -E '^(http|#)' | awk '!seen[$0]++' | sed '1s/^/#EXTM3U\n/' > everything-full.m3u
 
 # read the full file and remove all extra stuff, we just need the links
 cat everything-full.m3u | sed -n '/^#/!p' > everything-lite.m3u
@@ -21,6 +21,10 @@ cat everything-lite.m3u | shuf > randomized.m3u
 # sort the lite file
 cat everything-lite.m3u | sort > sorted.m3u
 
+# for new everything-repo files
+#for i in AA-*.txt ; do cat $i | awk '!seen[$0]++' | grep -B1 "http" | grep -A1 "EXTINF" | awk 'length>4' > A$i ; echo -e $i ; done
+cat everything.txt | awk '!seen[$0]++{if(p&&/^https?:\/\//){print l;print}if($0~/^#EXTINF:/){p=1;l=$0;next}{p=0}}' > every2.m3u
+
 # for everything-repo streams
 # copy all of the everything-full files to a folder and change their names to avoid substitution
 for i in *.m3u ; do cat $i >> every.txt ; done
@@ -30,3 +34,12 @@ cat every.txt | awk '!seen[$0]++' > every.m3u
 
 # remove stream info
 cat every.m3u | sed -n '/^#/!p' | sort > lite.m3u
+
+# for root playlists
+for f in *.m3u; do (head -n 1 "$f"; tail -n +2 "$f" | paste - - | shuf | tr '\t' '\n' | head -n 2000) > "$f.tmp" && mv "$f.tmp" "$f"; done
+
+# suffle without losing order
+for f in *.m3u; do (head -n 1 "$f"; tail -n +2 "$f" | paste - - | shuf | tr '\t' '\n') > "$f.tmp" && mv "$f.tmp" "$f"; done
+
+# cut to 2001 lines (1000 streams)
+for f in *; do [ -f "$f" ] && head -n 2001 "$f" > "$f.tmp" && mv "$f.tmp" "$f"; done
--- stuff/merged.sh
+++ stuff/merged.sh
@@ -4,7 +4,7 @@
 for F in * ; do NEWNAME=$(echo "$F" | tr '[:upper:]' '[:lower:]') ; mv "$F" "$NEWNAME" ; done
 
 # change all dashes in file names to underline
-for i in *-*.m3u ; do mv $i $(echo $i | sed 's/-/_/') ; done
+for i in *-*.m3u ; do mv $i $(echo $i | sed 's/-/_/g') ; done
 
 # combine all files 
 for i in $(find . -type f -name "*.m3u") ; do (cat "${i}"; echo) | grep -v "#EXTM3U" >> $(basename $i) ; done
@@ -12,16 +12,17 @@ for i in $(find . -type f -name "*.m3u") ; do (cat "${i}"; echo) | grep -v "#EXT
 # remove all the folders and put the m3u files in root to a new folder
 
 # remove duplicates
-for i in $(find . -type f -name "*.m3u") ; do cat $i | awk '!seen[$0]++' | grep -B1 "http" | grep -A1 "EXTINF" | awk 'length>4' > $(basename $i) ; echo -e $i ; done
+#for i in $(find . -type f -name "*.m3u") ; do cat $i | awk '!seen[$0]++' | grep -B1 "http" | grep -A1 "EXTINF" | awk 'length>4' > $(basename $i) ; echo -e $i ; done
+for i in $(find . -type f -name "*.m3u") ; do cat $i | awk '!seen[$0]++{if(p&&/^https?:\/\//){print l;print}if($0~/^#EXTINF:/){p=1;l=$0;next}{p=0}}' > $(basename $i) ; echo -e $i ; done
 
+# do all the 3 commands below at once
+find . -type f -name "*.m3u" -exec sh -c 'sed -i "s/\r$//" "$1" && sed -i "1s/^/#EXTM3U\n/" "$1" && sed -i "/^$/d" "$1"' _ {} \;
 # make all files linux compatible (still works on other os(es))
-for i in $(find . -type f -name "*.m3u") ; do sed -i 's/\r$//' $i ; done
-
+#for i in $(find . -type f -name "*.m3u") ; do sed -i 's/\r$//' $i ; done
 # add back "#EXTM3U" to files
-for i in $(find . -type f -name "*.m3u") ; do sed -i '1s/^/#EXTM3U\n/' $i ; done
-
+#for i in $(find . -type f -name "*.m3u") ; do sed -i '1s/^/#EXTM3U\n/' $i ; done
 # remove empty lines
-for i in $(find . -type f -name "*.m3u") ; do sed -i '/^$/d' $i ; done
+#for i in $(find . -type f -name "*.m3u") ; do sed -i '/^$/d' $i ; done
 
 # put all files into folders starting with the first character in their names 
 for i in *.m3u ; do dir=$(echo $i | cut -c 1 -) ; mkdir -p $dir ; mv $i $dir ; done
